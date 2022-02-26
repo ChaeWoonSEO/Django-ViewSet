48263aaa8ac158e169321114474982ca8c395af2
lot8229 <lot8229@kakao.com>
2022-02-26 18:15:32 +0900

    viewset 활용하여 view 작성

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from .models import Blog
+from .serializers import BlogSerializers
+from rest_framework import viewsets
 
-# Create your views here.
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializers
